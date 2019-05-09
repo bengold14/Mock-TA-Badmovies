4e91855230fc8f8ba2e1c6ef9f24847d987b340b
bengold14 <bengold14@gmail.com>
2019-05-09 11:47:24 -0400

    added storeMovie,deleteMovie and retrieveFavorites functionality

--- db/mongodb/index.js
+++ db/mongodb/index.js
@@ -7,26 +7,30 @@ var myMovies = Mongoose.model('badMovies',new Schema ({
   url: String,
 }))
 
-var storeMovie = function (movie) {
+var storeMovie = function (movie,callback) {
   var newFavorite = new myMovies ({
     name: movie.name,
     url: movie.url
   })
-  newFavorite.save(function (err){
+  newFavorite.save(function (err,success){
     if (err) {
       console.log('we errored in storing movie (index.js)',err)
+      callback(err,null)
     } else {
       console.log('we succeeded in storing movie (index.js)')
+      callback(null,success)
     }
   })
 }
 
-var deleteMovie = function (movie) {
-  myMovies.deleteOne({url: movie.url},function (err){
+var deleteMovie = function (movie,callback) {
+  myMovies.deleteOne({url: movie.url},function (err,success){
     if (err) {
       console.log('we errored in removing movie (index.js)',err)
+      callback(err,null)
     } else {
       console.log('we succeeded in removing movie (index.js)')
+      callback(null,success)
     }
   })
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request')
+var database = require('../db/mongodb/index.js')
 var app = express();
 
 // Sign up and get your moviedb API key here:
@@ -16,7 +17,16 @@ app.use(bodyParser.json());
 // Due to express, when you load the page, it doesn't make a get request to '/', it simply serves up the dist folder
 app.use(express.static(__dirname + '/../client/dist'));
 
-
+app.get('/favorites', function (req,res) {
+  apiHelpers.retrieveFavorites((err,data)=>{
+    if (err) {
+      console.log('error happened getting the favorites',err)
+      res.status(500).send()
+    } else {
+      res.status(200).send(data)
+    }
+  })
+})
 
 app.get('/genres', function(req, res) {
   apiHelpers.getGenres((err,data)=>{
@@ -43,14 +53,32 @@ app.get('/search', function(req, res) {
 
 
 app.post('/save', function(req, res) {
-
   //save movie as favorite
+  var movie = req.body.movie
+  database.storeMovie(movie,(err,success) => {
+    if (err) {
+      console.log('error happened saving the movie',err)
+      res.status(500).send()
+    } else {
+      res.status(200).send()
+    }
+  })
+
+
 
 });
 
 app.post('/delete', function(req, res) {
 
-  //remove movie from favorites
+  var movie = req.body.movie
+  database.deleteMovie(movie,(err,success) => {
+    if (err) {
+      console.log('error happened deleting the movie',err)
+      res.status(500).send()
+    } else {
+      res.status(200).send()
+    }
+  })
 
 });
 
